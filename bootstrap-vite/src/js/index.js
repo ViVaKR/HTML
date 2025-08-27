46876c7f2fca5d2b656503a3e32f3d76650b5c11
Kim Bum Jun <admin@vivabj.com>
2025-08-27 09:49:54 +0900

    Modified bootstrap-vite [index.html]

---
+++ bootstrap-vite/src/js/index.js
@@ -0,0 +1,23 @@
+
+
+function openDialog() {
+    const dialog = document.querySelector('dialog');
+    dialog.show();
+}
+
+function closeDialog() {
+    const dialog = document.querySelector('dialog');
+    dialog.close();
+}
+
+function drawRect() {
+    const canvas = document.getElementById("canvas1");
+    if (canvas.getContext) {
+        const ctx = canvas.getContext("2d");
+        ctx.fillStyle = "rgb(200, 0, 0)";
+        ctx.fillRect(0, 0, 50, 50);
+    }
+
+}
+
+drawRect();
