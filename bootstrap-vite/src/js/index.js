31eb226055ddeb46a2ac75809228365e19131d28
Kim Bum Jun <admin@vivabj.com>
2025-09-03 11:39:27 +0900

    Modified, bootstrap-vite/src/index.html, tags part 2

--- bootstrap-vite/src/js/index.js
+++ bootstrap-vite/src/js/index.js
@@ -20,3 +20,39 @@ function drawRect() {
 }
 
 drawRect();
+
+
+function showProduct() {
+
+    const details = document.getElementById("product-details");
+
+
+    details.addEventListener("click", function () {
+        const id = this.dataset.productId;
+        console.log(id)
+        const product = document.getElementById(id);
+        const name = product.dataset.name;
+        const price = product.dataset.price;
+        const stock = product.dataset.stock;
+        const color = product.dataset.color;
+
+        const msg =
+            `제품 이름: ${name}\n` +
+            `가격: ${price}\n` +
+            `재고: ${stock}\n` +
+            `색상: ${color}`;
+
+        console.log(msg);
+
+    });
+}
+
+showProduct();
+
+function demo1() {
+    const obj = {};
+    obj.prop = 3.14;
+    console.log(obj.prop);
+}
+
+demo1();
